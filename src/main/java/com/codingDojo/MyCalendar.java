1ddacfd2934e1df6602302d4bae3ba770b5c2dfd
patils <supriya.patil@cdk.com>
2020-05-13 19:20:51 +0530

    refactor code

--- src/main/java/com/codingDojo/MyCalendar.java
+++ src/main/java/com/codingDojo/MyCalendar.java
@@ -5,12 +5,10 @@ public class MyCalendar {
     public static boolean isLeapYear(final int year) {
         if (year <= 0)
             throw new IllegalArgumentException("Invalid year value");
-        if (year % 400 == 0)
-            return true;
-        if (year % 100 == 0)
-            return false;
-        if (year % 4 == 0)
-            return true;
-        return false;
+        return isDivisibleBy(year, 400) || !isDivisibleBy(year, 100) && isDivisibleBy(year, 4);
+    }
+
+    private static boolean isDivisibleBy(final int year, final int i) {
+        return year % i == 0;
     }
 }
